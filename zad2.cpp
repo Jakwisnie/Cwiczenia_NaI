645cd6ee775738a4eeaf7c28488954b154c58628
Jakwisnie <47974800+Jakwisnie@users.noreply.github.com>
2021-10-20 12:21:22 +0200

    update

--- zad2.cpp
+++ zad2.cpp
@@ -1,86 +1,105 @@
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <list>
-#include <random>
-#include <vector>
+    #include <functional>
+    #include <iomanip>
+    #include <iostream>
+    #include <list>
+    #include <random>
+    #include <vector>
 
-using namespace std;
+    using namespace std;
 
-random_device rd;
-mt19937 gen(rd()); 
+    random_device rd;
+    mt19937 gen(rd()); 
 
-ostream& operator<<(ostream& o, vector<double> v)
-{
-    for (auto e : v) {
-        o << std::fixed << std::setprecision(5) << "\t" << e;
-    }
-    return o;
-}
-
-vector<double> hill_climbing(function<double(vector<double>)> f, function<bool(vector<double>)> f_domain, vector<double> p0, int iterations)
-{
-    auto p = p0;
-    uniform_int_distribution<> distrib(0, p.size() - 1);
-    uniform_real_distribution<> distrib_r(-0.1, 0.1);
-
-    if (!f_domain(p)) throw std::invalid_argument("The p0 point must be in domain");
-    for (int i = 0; i < iterations; i++) {
-        auto p2 = p;
-
-        p[distrib(gen)] += distrib_r(gen);
-        double y2 = f(p2);
-        if (y2 < f(p)) {
-            p = p2;
+    ostream& operator<<(ostream& o, vector<double> v)
+    {
+        for (auto e : v) {
+            o << std::fixed << std::setprecision(5) << "\t" << e;
         }
+        return o;
     }
-    return p;
-}
 
-int main()
-{
-    auto function1 = [](vector<double> v) {
-        return 100 - (v[0] * v[0] + v[1] * v[1]);
-    };
+    vector<double> hill_climbing(function<double(vector<double>)> f, function<bool(vector<double>)> f_domain, vector<double> p0, int iterations)
+    {
+        auto p = p0;
+        uniform_int_distribution<> distrib(0, p.size() - 1);
+        uniform_real_distribution<> distrib_r(-0.1, 0.1);
 
-    auto ackley = [](vector<double> v) {
-        double x = v.at(0), y = v.at(1);
-        return -20 * exp(-0.2 * sqrt(0.5 * (x * x + y * y))) - exp(0.5 * (cos(2 * M_PI * x) + cos(2 * M_PI * y))) + M_E + 20.0;
-    };
+        if (!f_domain(p)) throw std::invalid_argument("The p0 point must be in domain");
+        for (int i = 0; i < iterations; i++) {
+            auto p2 = p;
 
-    auto ackley_domain = [](vector<double> v) {
+            p2[distrib(gen)] += distrib_r(gen);
+            if(!f_domain(p2)){
+            double y2 = f(p2);
+            if (y2 < f(p)) {
+                p = p2;
+            }
+            }
+        }
+        return p;
+    }
+
+    int main()
+    {
+         cout<<"Ktorą funkcje chcesz wykorzystac ? 1 - ackley , 2- himmelblau"<<endl;
+        int a;
+        cin>>a;
+
+        int b,c;
+        cout <<"Podaj pierwszy argument"<<endl;
+        cin>>b;
+        
+         cout <<"Podaj drugi argument"<<endl;
+        cin>>c;
+
+
+
+        auto function1 = [](vector<double> v) {
+            return 100 - (v[0] * v[0] + v[1] * v[1]);
+        };
+
+        auto ackley = [](vector<double> v) {
+            double x = v.at(0), y = v.at(1);
+            return -20 * exp(-0.2 * sqrt(0.5 * (x * x + y * y))) - exp(0.5 * (cos(2 * M_PI * x) + cos(2 * M_PI * y))) + M_E + 20.0;
+        };
+
+        auto ackley_domain = [](vector<double> v) {
+            return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
+        };
+
+        uniform_real_distribution<> distrib_r(-5, 5);
+        vector<double> ackley_p0 = {
+            distrib_r(b),
+            distrib_r(c),
+        };
+
+
+        auto himmelblau = [](vector <double> v){
+            double x = v.at(0),y= v.at(1);
+            return (x*x+y-11)*(x*x+y-11)+(x+y*y-7)*(x+y*y-7);
+        };
+        auto himmelblau_domain = [](vector <double> v){
         return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
-    };
+        };
+        
+        vector<double> himmelblau_p0 = {
+            distrib_r(b),
+            distrib_r(c),
+        };
 
-    uniform_real_distribution<> distrib_r(-5, 5);
-    vector<double> ackley_p0 = {
-        distrib_r(gen),
-        distrib_r(gen),
-    };
-
- auto result = hill_climbing(ackley, ackley_domain, ackley_p0, 100000);
- cout << result << " -> " << ackley(result) << endl;
-
-
-    auto matyas = [](vector <double> v){
-        double x = v.at(0),y= v.at(1);
-        return -0.26*(x*x -y*y)-0.48*x*y;
-    };
-    auto matyas_domain = [](vector <double> v){
-    return (abs(v[0]) <= 10) && (abs(v[1]) <= 10);
-    };
-     uniform_real_distribution<> distrib_r(-10, 10);
-    vector<double> matyas_p0 = {
-        distrib_r(gen),
-        distrib_r(gen),
-    };
-
-
- auto result =  hill_climbing(matyas, matyas_domain, matyas_p0, 10000);
-   cout << result << " -> " << matyas(result) << endl;
-   
-
+if(a==1){
     
-
-    return 0;
+     auto result = hill_climbing(ackley, ackley_domain, ackley_p0, 100000);
+    cout << result << " -> " << ackley(result) << endl;
 }
+else {
+    if(a==2){
+    auto result2 =  hill_climbing(himmelblau, himmelblau_domain, himmelblau_p0, 10000);
+    cout << result2 << " -> " << himmelblau(result) << endl;
+    }
+    else {
+        cout <<" zly argument";
+    }
+}
+        return 0;
+    }
