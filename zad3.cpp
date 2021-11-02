6594c3c94b2dd3aa8e2837bf686cfb7c51154846
Jakwisnie <47974800+Jakwisnie@users.noreply.github.com>
2021-11-02 20:05:23 +0100

    zad 3 oraz 4

--- zad3.cpp
+++ zad3.cpp
@@ -43,13 +43,7 @@ vector<double> hill_climbing(function<double(vector<double>)> f, function<bool(v
     }
     return p;
 }
-vector<double> simulated_annealing(
-    function<double(vector<double>)> f,
-    function<bool(vector<double>)> f_domain,
-    vector<double> p0,
-    int iterations,
-    function<vector<double>(vector<double>)> N,
-    function<double(int)> T)
+vector<double> simulated_annealing(function<double(vector<double>)> f, function<bool(vector<double>)> f_domain, vector<double> p0, int iterations, function<vector<double>(vector<double>)> N, function<double(int)> T)
 {
     auto s_current = p0;
     auto s_global_best = p0;
@@ -57,30 +51,43 @@ vector<double> simulated_annealing(
     //    uniform_int_distribution<> distrib(0, p.size() - 1);
     uniform_real_distribution<> u_k(0.0, 1.0);
 
-    if (!f_domain(s_current)) throw std::invalid_argument("The p0 point must be in domain");
+    if (!f_domain(s_current))
+        throw std::invalid_argument("The p0 point must be in domain");
 
-    for (int k = 0; k < iterations; k++) {
+    for (int k = 0; k < iterations; k++)
+    {
         auto s_next = N(s_current);
-        if (f(s_next) < f(s_current)) {
-            s_current = s_next;
-        } else {
-            double u = u_k(mt_generator);
-            if (u < exp(-abs(f(s_next) - f(s_current)) / T(k))) {
+        if (f_domain(s_next))
+        {
+            if (f(s_next) < f(s_current))
+            {
                 s_current = s_next;
-            } else {
-                // nothing...
+            }
+            else
+            {
+                double u = u_k(gen);
+                if (u < exp(-abs(f(s_next) - f(s_current)) / T(k)))
+                {
+                    s_current = s_next;
+                }
+                else
+                {
+                    // nothing...
+                }
             }
         }
-        if (f(s_current) < f(s_global_best)) {
+        if (f(s_current) < f(s_global_best))
+        {
             s_global_best = s_current;
         }
         //        cout << k << " " << f(s_current) << endl;
         cout << s_current << " " << f(s_current) << endl;
     }
+
     return s_global_best;
 }
 int main()
-{   
+{
     cout << "Ktory algorytm chcesz wykorzystac ? 1 - hill_climbing , 2- " << endl;
     int g;
     cin >> g;
@@ -107,106 +114,125 @@ int main()
     {
         return 100 - (v[0] * v[0] + v[1] * v[1]);
     };
-if(g == 1){
-    if (a == 1)
+    if (g == 1)
     {
-        auto ackley = [](vector<double> v)
+        if (a == 1)
         {
-            double x = v.at(0), y = v.at(1);
-            return -20 * exp(-0.2 * sqrt(0.5 * (x * x + y * y))) - exp(0.5 * (cos(2 * M_PI * x) + cos(2 * M_PI * y))) + M_E + 20.0;
-        };
-
-        auto ackley_domain = [](vector<double> v)
-        {
-            return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
-        };
-
-        uniform_real_distribution<> distrib_r(-5, 5);
-        vector<double> ackley_p0 = {
-            distrib_r(gen),
-            distrib_r(gen),
-        };
-        auto result = hill_climbing(ackley, ackley_domain, ackley_p0, 100000);
-        cout << result << " -> " << ackley(result) << endl;
-    }
-    else
-    {
-        if (a == 2)
-        {
-
-            auto himmelblau = [](vector<double> v)
-            {
-                double x = v.at(0), y = v.at(1);
-                return (x * x + y - 11) * (x * x + y - 11) + (x + y * y - 7) * (x + y * y - 7);
-            };
-            auto himmelblau_domain = [](vector<double> v)
-            {
-                return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
-            };
-            uniform_real_distribution<> distrib_r(-5, 5);
-            vector<double> himmelblau_p0 = {
-                distrib_r(gen),
-                distrib_r(gen),
-            };
-            auto result2 = hill_climbing(himmelblau, himmelblau_domain, himmelblau_p0, 10000);
-            cout << result2 << " -> " << himmelblau(result2) << endl;
-        }
-        else
-        {
-            cout << " zly argument";
-        }
-    
-    }
-    else{
-        if (g ==2){
             auto ackley = [](vector<double> v)
-        {
-            double x = v.at(0), y = v.at(1);
-            return -20 * exp(-0.2 * sqrt(0.5 * (x * x + y * y))) - exp(0.5 * (cos(2 * M_PI * x) + cos(2 * M_PI * y))) + M_E + 20.0;
-        };
-
-        auto ackley_domain = [](vector<double> v)
-        {
-            return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
-        };
-
-        uniform_real_distribution<> distrib_r(-5, 5);
-        vector<double> ackley_p0 = {
-            distrib_r(gen),
-            distrib_r(gen),
-        };
-        auto result = simulated_annealing(ackley, ackley_domain, ackley_p0, 100000);
-        cout << result << " -> " << ackley(result) << endl;
-    }
-    else
-    {
-        if (a == 2)
-        {
-
-            auto himmelblau = [](vector<double> v)
             {
                 double x = v.at(0), y = v.at(1);
-                return (x * x + y - 11) * (x * x + y - 11) + (x + y * y - 7) * (x + y * y - 7);
+                return -20 * exp(-0.2 * sqrt(0.5 * (x * x + y * y))) - exp(0.5 * (cos(2 * M_PI * x) + cos(2 * M_PI * y))) + M_E + 20.0;
             };
-            auto himmelblau_domain = [](vector<double> v)
+
+            auto ackley_domain = [](vector<double> v)
             {
                 return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
             };
+
             uniform_real_distribution<> distrib_r(-5, 5);
-            vector<double> himmelblau_p0 = {
+            vector<double> ackley_p0 = {
                 distrib_r(gen),
                 distrib_r(gen),
             };
-            auto result2 = simulated_annealing(himmelblau, himmelblau_domain, himmelblau_p0, 10000);
-            cout << result2 << " -> " << himmelblau(result2) << endl;
+            auto result = hill_climbing(ackley, ackley_domain, ackley_p0, 100000);
+            cout << result << " -> " << ackley(result) << endl;
+        }
+        else
+        {
+            if (a == 2)
+            {
+
+                auto himmelblau = [](vector<double> v)
+                {
+                    double x = v.at(0), y = v.at(1);
+                    return (x * x + y - 11) * (x * x + y - 11) + (x + y * y - 7) * (x + y * y - 7);
+                };
+                auto himmelblau_domain = [](vector<double> v)
+                {
+                    return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
+                };
+                uniform_real_distribution<> distrib_r(-5, 5);
+                vector<double> himmelblau_p0 = {
+                    distrib_r(gen),
+                    distrib_r(gen),
+                };
+                auto result2 = hill_climbing(himmelblau, himmelblau_domain, himmelblau_p0, 10000);
+                cout << result2 << " -> " << himmelblau(result2) << endl;
+            }
+            else
+            {
+                cout << " zly argument";
+            }
+        }
+    }
+    else
+    {
+        if (g == 2)
+        {
+            if (a == 1)
+            {
+                auto ackley = [](vector<double> v)
+                {
+                    double x = v.at(0), y = v.at(1);
+                    return -20 * exp(-0.2 * sqrt(0.5 * (x * x + y * y))) - exp(0.5 * (cos(2 * M_PI * x) + cos(2 * M_PI * y))) + M_E + 20.0;
+                };
+
+                auto ackley_domain = [](vector<double> v)
+                {
+                    return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
+                };
+
+                uniform_real_distribution<> distrib_r(-5, 5);
+                vector<double> ackley_p0 = {
+                    distrib_r(gen),
+                    distrib_r(gen),
+                };
+                auto result = simulated_annealing(
+        ackley, ackley_domain, ackley_p0, 10000,
+        [](auto p) {
+            normal_distribution<double> n(0.0, 0.3);
+            for (auto& e : p) {
+                e = e + n(mt_generator);
+            }
+            return p;
+        },
+        [](int k) { return 1000.0 / k; });
+    cout << result << " -> " << ackley(result) << endl;
+            }
+            else
+            {
+                if (a == 2)
+                {
+
+                    auto himmelblau = [](vector<double> v)
+                    {
+                        double x = v.at(0), y = v.at(1);
+                        return (x * x + y - 11) * (x * x + y - 11) + (x + y * y - 7) * (x + y * y - 7);
+                    };
+                    auto himmelblau_domain = [](vector<double> v)
+                    {
+                        return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
+                    };
+                    uniform_real_distribution<> distrib_r(-5, 5);
+                    vector<double> himmelblau_p0 = {
+                        distrib_r(gen),
+                        distrib_r(gen),
+                    };
+                    auto result2 = simulated_annealing(himmelblau, himmelblau_domain, himmelblau_p0, 10000);
+                    cout << result2 << " -> " << himmelblau(result2) << endl;
+                }
+                else
+                {
+                    cout << " zly argument";
+                }
+            }
         }
         else
         {
             cout << " zly argument";
         }
-    
-        }
+
+       
     }
-}else{cout << " zly argument";}
-    return 0;
+ return 0;
 }
--- zad4.cpp
+++ zad4.cpp
@@ -1,198 +1,131 @@
-#include "../lab06/02.cpp"
-#include "../genetic/classes.cpp"
-#include <memory>
+#include <chrono>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <random>
+#include <tuple>
+#include <vector>
 
-vector<Organism> initOrganism(vector<Organism> organisms, int genesNum = 128, int n = 1)
+using genotype_t = std::vector<char>;
+using population_t = std::vector<genotype_t>;
+using population_fitness_t = std::vector<double>;
+
+
+std::random_device random_dev;
+std::default_random_engine random_engine(random_dev());
+std::uniform_real_distribution<double> double_random_dist(0.0, 1.0);
+std::uniform_int_distribution<char> rand_01(0, 1);
+
+
+std::vector<int> selection_roulette(population_fitness_t fitnesses)
 {
-    for (int x = 0; x < n; x++)
-    {
-        Genotype genotype;
-
-        //np. 1 genotyp = 32 chromosomy(16 par po 2 geny)
-        //TODO: sensowne przydzielenie par chromosomów na podstawie wartości `genesNum`
-        for (int i = 0; i < 16; i++)
-        {
-            vector<Gene> genes;
-            for (int j = 0; j < 8; j++)
-            {
-                Gene gene;
-                gene.initGene();
-                genes.push_back(gene);
-            }
-            Chromosome chromosome(genes);
-            // chromosome.grayCode();
-            genotype.chromosomes.push_back(chromosome);
-        }
-        genotype.calculateX();
-        genotype.calculateY();
-        Organism organism(genotype);
-        organisms.push_back(organism);
-    }
-    return organisms;
-};
-
-vector<Organism> fitness(vector<Organism> organisms, FunctionToOptI *goal)
-{
-    for (int i = 0; i < organisms.size(); i++)
-    {
-        organisms.at(i).genotype.calculateX();
-        organisms.at(i).genotype.calculateY();
-        // cout << "goal " << goal->calculate(organisms.at(i).genotype.x,organisms.at(i).genotype.y) << endl;
-        organisms.at(i).fitness = 1 / (1 + abs(goal->calculate(organisms.at(i).genotype.x, organisms.at(i).genotype.y)));
-    }
-    return organisms;
-}
-
-void showPopulationFitness(Population population)
-{
-    cout << "\nOrganisms fitness: \n[";
-    for (int i = 0; i < population.organisms.size(); i++)
-    {
-        cout << "  {" << population.organisms.at(i).fitness << "}";
-        if (i % 4 == 0 && i != 0)
-            cout << '\n';
-    }
-    cout << " ]\n";
-}
-
-void showPopulation(Population population)
-{
-    for (int i = 0; i < population.organisms.size(); i++)
-    {
-        cout << "Organism [" << i + 1 << "]\n------\n";
-        for (int j = 0; j < 1; j++)
-        {
-            cout << "Genotype \n-------\n";
-            for (int k = 0; k < population.organisms.at(i).genotype.chromosomes.size(); k++)
-            {
-                cout << "\tGenes of Chromosome [" << k + 1 << "]\n";
-                cout << "\t";
-                for (int l = 0; l < population.organisms.at(i).genotype.chromosomes.at(k).genes.size(); l++)
-                {
-                    cout << population.organisms.at(i).genotype.chromosomes.at(k).genes.at(l).value << " ";
-                }
-                cout << "\n";
-            }
-        }
-    }
-}
-
-Population crossover(Population population, double p_crossover)
-{
-    Population ret_pop;
-    uniform_real_distribution<double> r_pcross(0.0, 1.0);
-    for (int i = 0; i < (population.organisms.size() - 1); i += 2)
-    {
-        Organism a = population.organisms.at(i);
-        Organism b = population.organisms.at(i + 1);
-
-        if (r_pcross(rdev) < p_crossover)
-        {
-            uniform_int_distribution<int>(0, a.genotype.chromosomes.size() - 1);
-            auto crossover_p = dist(rdev);
-            for (int g = crossover_p; g < a.genotype.chromosomes.size(); g++)
-            {
-                try
-                {
-                    swap(a.genotype.chromosomes.at(g).genes, b.genotype.chromosomes.at(g).genes);
-                }
-                catch (exception e)
-                {
-                    cout << e.what();
-                }
-            }
-        }
-        ret_pop.organisms.push_back(a);
-        ret_pop.organisms.push_back(b);
-    }
-    return ret_pop;
-};
-
-Population get_crossover_one_point(Population pop, double p_crossover = 0.2)
-{
-    Population crossover_one_point = crossover(pop, p_crossover);
-    return crossover_one_point;
-};
-
-Population mutation_uniform(Population population, double p_mutation)
-{
-    Population ret_pop;
-    uniform_real_distribution<double> r_mut(0.0, 1.0);
-    for (int i = 0; i < population.organisms.size(); i++)
-    {
-        Organism a = population.organisms.at(i);
-        for (int g = 0; g < a.genotype.chromosomes.size(); g++)
-        {
-            if(r_mut(rdev) < p_mutation){
-                vector<Gene> genes = a.genotype.chromosomes.at(g).genes;
-                if(a.genotype.chromosomes.at(g).genes.at(genes.size()/2).value == '1'){
-                    a.genotype.chromosomes.at(g).genes.at(genes.size()/2).value = '0';
-                }else {
-                    a.genotype.chromosomes.at(g).genes.at(genes.size()/2).value = '1';
-                }
-            }
-        }
-        ret_pop.organisms.push_back(a);
-    }
-    return ret_pop;
-};
-
-Population get_mutation_uniform(Population pop, double p_mutation = 0.1)
-{
-    Population mutationPop = mutation_uniform(pop, p_mutation);
-    return mutationPop;
-};
-
-Population selection_roulette(Population population)
-{
-    Population selected_population;
-    double sum_fitness = accumulate(population.organisms.begin(), population.organisms.end(), 0.0, [](auto a, auto b) {
-        return a + b.fitness;
-    });
-    cout << "sum fit: " << sum_fitness << endl;
-
-    uniform_real_distribution<double> dist(0.0, sum_fitness);
-    for (int x = 0; x < population.organisms.size(); x++)
-    {
-        double randomVal = dist(rdev);
-        double s = 0.0;
-        Organism selected = population.organisms.at(0);
-        for (unsigned int i = 0; i < population.organisms.size(); i++)
-        {
-            s += population.organisms.at(i).fitness;
-            if (randomVal < s)
-            {
-                selected_population.organisms.push_back(population.organisms.at(i));
+    std::vector<int> ret;
+    double sum_fit = std::accumulate(fitnesses.begin(), fitnesses.end(), 0.0);
+    std::uniform_real_distribution<double> random_distr(0.0, sum_fit);
+    for (int j = 0; j < fitnesses.size(); j++) {
+        double rand_selected = random_distr(random_engine);
+        double s = fitnesses[0];
+        int selected_i = fitnesses.size() - 1;
+        for (int i = 0; i < fitnesses.size() - 1; i++) {
+            if (s > rand_selected) {
+                selected_i = i;
                 break;
             }
+            s = s + fitnesses[i + 1];
         }
+
+        ret.push_back(selected_i);
     }
-    return selected_population;
-};
+    return ret;
+}
+
+std::pair<genotype_t, genotype_t> empty_crossover_f(genotype_t a, genotype_t b)
+{
+    return {a, b};
+}
+genotype_t empty_mutation_f(genotype_t a, double)
+{
+    return a;
+}
+
+
+population_t genetic_algorithm(
+    std::function<double(genotype_t)> fitnes_f,
+    const population_t init_population,
+    double crossover_probability,
+    double mutation_probability,
+    std::function<std::pair<genotype_t, genotype_t>(genotype_t, genotype_t)> crossover_f,
+    std::function<genotype_t(genotype_t, double)> mutation_f,
+    std::function<std::vector<int>(population_fitness_t)> select_f,
+    std::function<bool(population_t, population_fitness_t, int)> termination_cond = [](auto, auto, auto i) {
+        return i < 100;
+    })
+{
+    int iteration = 0;
+    population_t population = init_population;
+    population_fitness_t population_fit;
+    for (auto& gene : population)
+        population_fit.push_back(fitnes_f(gene));
+    while (termination_cond(population, population_fit, iteration)) {
+        std::vector<int> parent_pop_i = select_f(population_fit);
+
+        population_t offspring_pop(parent_pop_i.size());
+        for (int i = 1; i < parent_pop_i.size(); i += 2) {
+            int parent_idx_0 = parent_pop_i[i - 1];
+            int parent_idx_1 = parent_pop_i[i];
+            if (double_random_dist(random_engine) < crossover_probability) {
+                auto [a, b] = crossover_f(population[parent_idx_0], population[parent_idx_1]);
+                offspring_pop[i - 1] = a;
+                offspring_pop[i] = b;
+            } else {
+                offspring_pop[i - 1] = population[parent_idx_0];
+                offspring_pop[i] = population[parent_idx_1];
+            }
+        }
+
+        for (int i = 0; i < parent_pop_i.size(); i++) {
+            offspring_pop[i] = mutation_f(offspring_pop[i], mutation_probability);
+        }
+
+        population = offspring_pop;
+
+        for (int i = 0; i < population.size(); i++)
+            population_fit[i] = fitnes_f(population[i]);
+        iteration++;
+    }
+
+    return population;
+}
+
+
+/////// problem implementation
+
+double one_max(genotype_t gene)
+{
+    return std::accumulate(gene.begin(), gene.end(), 0.0);
+}
 
 int main()
 {
-    Population population;
-    // shared_ptr<Levi> goal(new Levi());
-
-    FunctionToOptI *goal = new Levi();
-    population.organisms = initOrganism(population.organisms, 128, 20);
-      population.organisms = fitness(population.organisms, goal);
-   
-    ofstream fileTxt;
-    fileTxt.open("./population.txt");
-    for (int i = 0; i < 5; i++)
-    {
-        showPopulationFitness(population);
-        population = selection_roulette(population);
-        population = get_crossover_one_point(population);
-        population = get_mutation_uniform(population);
-        population.organisms = fitness(population.organisms, goal);
-        for(int j = 0; j < population.organisms.size(); j++){
-            fileTxt << i << " "  << j << " " << population.organisms.at(j).fitness << endl;
-        }
-        
+    population_t init_pop(10);
+    for (auto& p : init_pop) {
+        for (int i = 0; i < 20; i++)
+            p.push_back(rand_01(random_engine));
     }
 
-    // showPopulation(population);
+    auto debug_term_cond = [](auto pop, auto fit, auto i) {
+        std::cout << i << ":";
+        for (auto e : fit) {
+            std::cout << " " << e;
+        }
+        std::cout << std::endl;
+        return i < 100;
+    };
+
+    auto result = genetic_algorithm(
+        one_max,
+        init_pop,
+        0.8, 0.001, empty_crossover_f, empty_mutation_f, selection_roulette,debug_term_cond);
+    return 0;
 }
